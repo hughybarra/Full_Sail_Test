2ae8e2c4ab80c3509e5b182622caf244e9ca3b27
Hugh Ybarra <hugh.ybarra@gmaillcom>
2014-07-18 19:15:30 -0400

    fixed fire fox bug yaya

--- public/js/main.js
+++ public/js/main.js
@@ -2,11 +2,31 @@ $( document ).ready(function() {
   	// Delete when ready
   	console.log("main.js is loading");
 
-
   	// hooking up the bxslider
 	$('.bxslider').bxSlider({
 		auto: true,
 		controls: false,
 		autoHover: true,
+	});
+
+	/* form click function */
+	$('.form_button').click(function(e){
+		// simple aler for functionality.
+		alert('Your Form was submitted');
+		e.preventDefault();
+		e.stopPropogation();
+	});
+
+	/* Kornify just for fun ^_^ */
+	var kkeys = [], konami = "38,38,40,40,37,39,37,39,66,65";
+	$(document).keydown(function(e) {
+	  kkeys.push( e.keyCode );
+	  if ( kkeys.toString().indexOf( konami ) >= 0 ){
+	    $(document).unbind('keydown',arguments.callee);
+	    $.getScript('http://www.cornify.com/js/cornify.js',function(){
+	      cornify_add();
+	      $(document).keydown(cornify_add);
+	    });
+	  }
 	});
 });// end doc ready function
